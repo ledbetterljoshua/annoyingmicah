78eb08b723e7921d6c274f3556a3833508e769c8
Micah R Ledbetter <mrled@younix.us>
2016-05-10 13:22:58 -0500

    Improve code readability and correctness
    - Properly calculate ordinal numbers
    - Add generic function to number prototype

--- annoyingmicah.js
+++ annoyingmicah.js
@@ -1,32 +1,20 @@
 //inital code written by Kyrsten Iley‎ 
 
+Number.prototype.ordinal = function(){
+    var numStr = this.toString();
+    var ord = '';
+    switch (numStr.slice(-1)) {
+        case '1': ord = numStr.slice(-2) === '11' ? 'th' : 'st'; break;
+        case '2': ord = numStr.slice(-2) === '12' ? 'th' : 'nd'; break;
+        case '3': ord = numStr.slice(-2) === '13' ? 'th' : 'rd'; break;
+        default: ord = 'th'; break;
+    }
+    return this.toString() + ord;
+};
+
 function annoyingMicahFilter(message) {
-    var d = new Date();
-    d = d.getDate().toString();
-    var f_d;
-    var o = [];
-    for(i=0;i < d.length; i++) {
-        o.push(+d.charAt(i));
-    }
-
-    var j = function(n) {
-        if(o[n] == 0 || o[n] == 4 || o[n] == 5 || o[n] == 6 || o[n] == 7 || o[n] == 8 || o[n] == 9) {f_d = f_d || d+"th";}
-        else if(o[n] == 1) {f_d = f_d || d+"st"}
-        else if(o[n] == 2) {f_d = f_d || d+"nd"}
-        else if(o[n] == 3) {f_d = f_d || d+"rd"}
-    }
-
-    if (o.length == 1) {
-        j(0);
-    }
-    else if (o.length == 2) {
-        j(1);
-    }
-    if(o[0] == 1 && o.length == 2) {
-        //if the number is a teen, always use "th"
-        f_d = d+"th";
-    }
-
-    return "MAY THE " + f_d + " BE WITH YOU!";
-}
-annoyingMicahFilter()
+    var today = new Date();
+    var dd = today.getDate().ordinal
+    return "MAY THE " + today.getDate().ordinal() + " BE WITH YOU!";
+};
+console.log(annoyingMicahFilter());
